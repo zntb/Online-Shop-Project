eec8e136c723f346c41e0fba26835362e7a75da1
zntb <zenetibi@yahoo.com>
2023-10-08 09:31:59 +0300

    Create signup form to signup.ejs

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 
-const authController = require("../controller/auth.controller");
+const authController = require("../controllers/auth.controller");
 
 const router = express.Router();
 
