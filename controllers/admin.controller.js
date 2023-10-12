0b546b6daa5860b7a6c9827f1584678dd42a0a70
zntb <zenetibi@yahoo.com>
2023-10-12 12:37:15 +0300

    Add admin.controller.js and all-products.ejs, new-product.ejs to admin page, and add form to that page

---
+++ controllers/admin.controller.js
@@ -0,0 +1,18 @@
+function getProducts(req, res) {
+    res.render("admin/products/all-products")
+}
+
+function getNewProduct(req, res) {
+    res.render("admin/products/new-product")
+}
+
+function createNewProduct(params) {
+    //
+}
+
+module.exports = {
+    getProducts: getProducts,
+    getNewProduct: getNewProduct,
+    createNewProduct: createNewProduct
+}
+
