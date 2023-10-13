26e7258d54bc3b3eca53e8a1fee36c3afbc8d2eb
zntb <zenetibi@yahoo.com>
2023-10-13 07:35:52 +0300

    Add product model and storing products in the database

--- controllers/admin.controller.js
+++ controllers/admin.controller.js
@@ -1,3 +1,5 @@
+const Product = require("../models/product.model");
+
 function getProducts(req, res) {
   res.render("admin/products/all-products");
 }
@@ -6,7 +8,19 @@ function getNewProduct(req, res) {
   res.render("admin/products/new-product");
 }
 
-function createNewProduct(req, res) {
+async function createNewProduct(req, res, next) {
+  const product = new Product({
+    ...req.body,
+    image: req.file.filename,
+  });
+
+  try {
+    await product.save();
+  } catch (error) {
+    next(error);
+    return;
+  }
+
   res.redirect("/admin/products");
 }
 
