7448a1aacf1451a1270703c7b016a62a74921d4f
zntb <zenetibi@yahoo.com>
2023-10-15 10:38:01 +0300

    Add protect-routes.js, 401.ejs, 403.ejs, 404.ejs and fix route protection

--- middlewares/error-handler.js
+++ middlewares/error-handler.js
@@ -1,6 +1,10 @@
 function handleErrors(error, req, res, next) {
   console.log(error);
   res.status(500).render("shared/500");
+
+  if (error.code === 404) {
+    return res.status(404).render("shared/404");
+  }
 }
 
 module.exports = handleErrors;
