32eaeb5a6446a94aed701c12336795684b5d0e1f
zntb <zenetibi@yahoo.com>
2023-10-16 08:08:05 +0300

    Configure the display of cart quantity

--- models/cart.model.js
+++ models/cart.model.js
@@ -1,6 +1,8 @@
 class Cart {
-  constructor(items = []) {
+  constructor(items = [], totalQuantity = 0, totalPrice = 0) {
     this.items = items;
+    this.totalQuantity = totalQuantity;
+    this.totalPrice = totalPrice;
   }
 
   addItem(product) {
@@ -15,10 +17,17 @@ class Cart {
         cartItem.quantity = cartItem + 1;
         cartItem.totalPrice = cartItem.totalPrice + product.price;
         this.items[i] = cartItem;
+
+        this.totalQuantity++; // this.totalQuantity + 1
+        this.totalPrice += product.price; // this.totalPrice + product.price
         return;
       }
     }
 
     this.items.push(cartItem);
+    this.totalQuantity++;
+    this.totalPrice += product.price;
   }
 }
+
+module.exports = Cart;
