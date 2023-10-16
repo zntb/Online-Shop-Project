c74d2ebaa24841cbe169f813e47d3c83ac458006
zntb <zenetibi@yahoo.com>
2023-10-16 12:50:10 +0300

    Configure updating cart items

--- controllers/cart.controller.js
+++ controllers/cart.controller.js
@@ -24,7 +24,28 @@ async function addCartItem(req, res, next) {
   });
 }
 
+function updateCartItem(req, res) {
+  const cart = res.locals.cart;
+
+  const updatedItemData = cart.updateItem(
+    req.body.productId,
+    req.body.quantity
+  );
+
+  req.session.cart = cart;
+
+  res.json({
+    message: "Item updated!",
+    updatedCartData: {
+      newTotalQuantity: cart.totalQuantity,
+      newTotalPrice: cart.totalPrice,
+      updatedItemPrice: updatedItemData.updatedItemPrice,
+    },
+  });
+}
+
 module.exports = {
   addCartItem: addCartItem,
   getCart: getCart,
+  updateCartItem: updateCartItem,
 };
