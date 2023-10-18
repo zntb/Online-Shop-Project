41fb0e74cbfe4c912f172100020575f276abb90b
zntb <zenetibi@yahoo.com>
2023-10-18 11:13:43 +0300

    Fix cart badge display in mobile version, add not-found.js and configure invalid page error handler

--- app.js
+++ app.js
@@ -1,31 +1,32 @@
-const path = require('path');
+const path = require("path");
 
-const express = require('express');
-const csrf = require('csurf');
-const expressSession = require('express-session');
+const express = require("express");
+const csrf = require("csurf");
+const expressSession = require("express-session");
 
-const createSessionConfig = require('./config/session');
-const db = require('./data/database');
-const addCsrfTokenMiddleware = require('./middlewares/csrf-token');
-const errorHandlerMiddleware = require('./middlewares/error-handler');
-const checkAuthStatusMiddleware = require('./middlewares/check-auth');
-const protectRoutesMiddleware = require('./middlewares/protect-routes');
-const cartMiddleware = require('./middlewares/cart');
-const updateCartPricesMiddleware = require('./middlewares/update-cart-prices');
-const authRoutes = require('./routes/auth.routes');
-const productsRoutes = require('./routes/products.routes');
-const baseRoutes = require('./routes/base.routes');
-const adminRoutes = require('./routes/admin.routes');
-const cartRoutes = require('./routes/cart.routes');
-const ordersRoutes = require('./routes/orders.routes');
+const createSessionConfig = require("./config/session");
+const db = require("./data/database");
+const addCsrfTokenMiddleware = require("./middlewares/csrf-token");
+const errorHandlerMiddleware = require("./middlewares/error-handler");
+const checkAuthStatusMiddleware = require("./middlewares/check-auth");
+const protectRoutesMiddleware = require("./middlewares/protect-routes");
+const cartMiddleware = require("./middlewares/cart");
+const updateCartPricesMiddleware = require("./middlewares/update-cart-prices");
+const notFoundMiddleware = require("./middlewares/not-found");
+const authRoutes = require("./routes/auth.routes");
+const productsRoutes = require("./routes/products.routes");
+const baseRoutes = require("./routes/base.routes");
+const adminRoutes = require("./routes/admin.routes");
+const cartRoutes = require("./routes/cart.routes");
+const ordersRoutes = require("./routes/orders.routes");
 
 const app = express();
 
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
-app.use(express.static('public'));
-app.use('/products/assets', express.static('product-data'));
+app.use(express.static("public"));
+app.use("/products/assets", express.static("product-data"));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
@@ -43,10 +44,11 @@ app.use(checkAuthStatusMiddleware);
 app.use(baseRoutes);
 app.use(authRoutes);
 app.use(productsRoutes);
-app.use('/cart', cartRoutes);
-app.use(protectRoutesMiddleware);
-app.use('/orders', ordersRoutes);
-app.use('/admin', adminRoutes);
+app.use("/cart", cartRoutes);
+app.use("/orders", protectRoutesMiddleware, ordersRoutes);
+app.use("/admin", protectRoutesMiddleware, adminRoutes);
+
+app.use(notFoundMiddleware);
 
 app.use(errorHandlerMiddleware);
 
@@ -55,6 +57,6 @@ db.connectToDatabase()
     app.listen(3000);
   })
   .catch(function (error) {
-    console.log('Failed to connect to the database!');
+    console.log("Failed to connect to the database!");
     console.log(error);
   });
--- public/scripts/cart-management.js
+++ public/scripts/cart-management.js
@@ -1,7 +1,7 @@
 const addToCartButtonElement = document.querySelector(
   "#product-details button"
 );
-const cartBadgeElement = document.querySelector(".nav-items .badge");
+const cartBadgeElements = document.querySelectorAll(".nav-items .badge");
 
 async function addToCart() {
   const productId = addToCartButtonElement.dataset.productid;
@@ -32,7 +32,10 @@ async function addToCart() {
   const responseData = await response.json();
 
   const newTotalQuantity = responseData.newTotalItems;
-  cartBadgeElement.textContent = newTotalQuantity;
+
+  for (const cartBadgeElement of cartBadgeElements) {
+    cartBadgeElement.textContent = newTotalQuantity;
+  }
 }
 
 addToCartButtonElement.addEventListener("click", addToCart);
